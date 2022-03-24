8966b8dca212de79cfaf53444ba456a5412e7a72
CLAW <christopher.lawn@yahoo.com>
2022-03-24 14:54:25 -0700

    updated & added structure and notes to App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        {/* Nav component featuring nav categories */}
       </header>
+
+      <main>
+        {/* About Me content.  About Me section is default homepage view! */}
+        {/* Portfolio/project cards content */}
+        {/* Contact form content */}
+      </main>
+
+      <footer className="footer">
+        {/* GitHub link */}
+        {/* LinkedIn link */}
+      </footer>
     </div>
   );
 }
