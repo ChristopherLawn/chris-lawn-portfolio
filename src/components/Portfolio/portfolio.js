8969d3bb63802fcb04f3970e885205c0e8a61bc7
CLAW <christopher.lawn@yahoo.com>
2022-04-21 16:36:27 -0700

    Added team project 'Mall Develop' to Portfolio page

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -3,10 +3,17 @@ import { Row, Container, Col, Card, Button, Stack } from 'react-bootstrap'
 export default function Portfolio() {
     const projectArrayOne = [
         {
+            title: "Team Project: Mall Direct",
+            link: 'https://mall-direct.herokuapp.com/',
+            image: 'https://github.com/knharman/mall-directory/blob/feature/README/client/assets/mall-direct-developer-dashboard.jpg?raw=true',
+            description: 'A great place to view or advertise your local shopping centers, Mall Direct is a dual-purpose website that serves as an accessible online mall guide for consumers and an easy-to-use content management system for mall developers.',
+            githubLink: 'https://github.com/knharman/mall-directory'
+        },
+        {
             title: "Team Project: LEARN IT",
             link: 'https://damp-journey-96412.herokuapp.com/',
             image: 'https://github.com/ChristopherLawn/learn-code-work/blob/main/assets/images/learn-it-homepage.jpg?raw=true',
-            description: 'An interactive educational forum for exploring web development languages!',
+            description: 'An interactive educational forum for exploring web development languages. Choose a language to view more about the history and background. Create an account to join the conversation and share personal anecdotes and online resources, view comment analytics, and engage with the larger LEARN IT community!',
             githubLink: 'https://github.com/ChristopherLawn/learn-code-work'
         },
         {
@@ -15,30 +22,23 @@ export default function Portfolio() {
             image: 'https://user-images.githubusercontent.com/91863054/160073211-b34cab41-b939-445f-86d0-f94a63a71465.png?raw=true',
             description: 'A free-to-use site that simplifies date nights and speeds up decision-making! Use Chew View to search restaurants by zip code and cuisine type & search for movies based on keywords and titles.',
             githubLink: 'https://github.com/ChristopherLawn/team-1-dinner-movie'
-        },
+        }
+    ];
+
+    const projectArrayTwo = [
         {
             title: "TechCollude",
             link: 'https://shrouded-bayou-81487.herokuapp.com/',
             image: 'https://github.com/ChristopherLawn/tech-collude/blob/main/assets/tech-collude-homepage.jpg?raw=true',
             description: 'An engaging tech blog site where users can create posts, leave comments on posts, and edit and delete posts using a simple and straightforward interface.',
             githubLink: 'https://github.com/ChristopherLawn/tech-collude'
-        }
-    ];
-
-    const projectArrayTwo = [
+        },
         {
             title: "International Weather Dashboard",
             link: 'https://christopherlawn.github.io/weather-dashboard/',
             image: 'https://user-images.githubusercontent.com/91863054/146675671-d16f3c24-dc24-4c27-8524-116b320b79d3.PNG?raw=true',
             description: 'A simple weather app where users can search for any city to see information about the current day and view the upcoming 5-day forecast. Reports include conditions, temperature, humidity levels, wind speed, and UVI index.',
             githubLink: 'https://github.com/ChristopherLawn/weather-dashboard'
-        },
-        {
-            title: "Work Day Scheduler",
-            link: 'https://christopherlawn.github.io/work-day-scheduler/',
-            image: 'https://user-images.githubusercontent.com/91863054/145012462-44f1da1d-edde-4e45-9c2e-a45040c79b64.PNG?raw=true',
-            description: 'A daily task scheduler that conveniently shows the user the current day and time and color-codes each hour to let the user know which tasks are overdue and which tasks are coming up!',
-            githubLink: 'https://github.com/ChristopherLawn/work-day-scheduler'
         },
         {
             title: "JavaScript Code Quiz",
